cc8b34c1c75443dd819b4358a2116ae2b294657c
nico <hylczp@163.com>
2017-02-22 15:43:36 +0800

    First commit.

---
+++ app/src/main/java/com/example/administrator/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//¹þ¹þ
+//·ÖÖ§
+        setContentView(R.layout.activity_main);
+    }
+}
